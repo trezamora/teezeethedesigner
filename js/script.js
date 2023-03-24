19285e443b0c9faff77a54f422c88d30f96e990f
trezamora <tzamora@hawaii.edu>
2023-03-23 16:15:35 -1000

    added new featured section

--- js/script.js
+++ js/script.js
@@ -19,6 +19,13 @@
 //     };
 // };
 
+// ON HOVER VIDEO PLAY
+const videos = document.querySelectorAll(".project-video");
+
+for (const video of videos) {
+    video.addEventListener('mouseover', function () { video.play() }, false);
+    video.addEventListener('mouseout', function () { video.pause() }, false);
+}
 
 // CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
 const io_options = {
